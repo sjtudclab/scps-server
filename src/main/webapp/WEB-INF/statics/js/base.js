d2ef7be5cb96bc0532570d4892d8d5e208399c17
Kaiyang Lv <kevinlui598@gmail.com>
2015-11-28 11:28:57 +0800

    Hack Ambari web into scps-server

---
+++ src/main/webapp/WEB-INF/statics/js/base.js
@@ -0,0 +1,44 @@
+$(function(){
+    function loginAm() {
+	 $.ajax({
+                type: "POST",
+                url: "http://192.168.1.111/ambari",
+                async: false,
+                beforeSend: function(xhr){
+                        xhr.setRequestHeader("Authorization",btoa("admin:admin"));
+                        xhr.setRequestHeader("X-Requested-By","ambari");
+                        xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+                },
+                success: function(){
+                        console.log("success");
+                }
+        });
+    }
+    loginAm();
+
+    state = {
+	START: 0,
+	FINISH: 1
+    };
+    function getState() {
+        // 0: start                                                                                                                              
+        // 1: finish                                                                                                                             
+        var len = $('#amIframe').contents().find('.services-menu.well.span2.service-menu-width').length;
+        if (len > 0)
+            return state.FINISH;
+        else
+            return state.START;
+    }
+    setInterval(function() {
+	switch(getState()) {
+            case 0:
+                $('#deploy').css('font-weight', 'bold');
+	        $('#manage').css('font-weight', 'normal');
+                break;
+            case 1:
+                $('#manage').css('font-weight', 'bold');
+	        $('#deploy').css('font-weight', 'normal');
+	        break;
+	    }
+    },1000);
+});
