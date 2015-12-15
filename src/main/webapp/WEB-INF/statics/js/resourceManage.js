38a0b9636cb25a10ef1991de008d0c34af4a4899
Kaiyang Lv <kevinlui598@gmail.com>
2015-12-15 20:36:26 +0800

    ResourceManage updateExpand done.

--- src/main/webapp/WEB-INF/statics/js/resourceManage.js
+++ src/main/webapp/WEB-INF/statics/js/resourceManage.js
@@ -20,13 +20,7 @@ $(function() {
                 $('.serviceAutoExpand2 .form-control:eq(4)').val(schema_setting_auto.nodeTime);
                 $('.serviceAutoExpand2 .bs-example .dropdown-menu:eq(0) li a:eq(' + schema_setting_auto.vm[0] + ')').click()
                 $('.serviceAutoExpand2 .bs-example .dropdown-menu:eq(1) li a:eq(' + schema_setting_auto.vm[1] + ')').click()
-            }
-        });
-        $.ajax({
-            type: "GET",
-            url: "/getExpand/1",
-            cache: false,
-            success: function(data){
+
                 var schema_setting_manual = $.parseJSON(data.schema_setting_manual);
                 $('.serviceAutoExpand3 .form-control:eq(0)').val(schema_setting_manual.nodeNum);
                 $('.serviceAutoExpand3 .bs-example .dropdown-menu:eq(0) li a:eq(' + schema_setting_manual.vm[0] + ')').click();
@@ -44,6 +38,7 @@ $(function() {
         var data = {};
         var threshold_setting = {};
         var schema_setting_auto = {};
+        var schema_setting_manual = {};
 
         threshold_setting.memory = $('.serviceAutoExpand2 .form-control:eq(0)').val();
         threshold_setting.workload = $('.serviceAutoExpand2 .form-control:eq(1)').val();
@@ -51,9 +46,40 @@ $(function() {
 
         schema_setting_auto.nodeNum = $('.serviceAutoExpand2 .form-control:eq(3)').val();
         schema_setting_auto.nodeTime = $('.serviceAutoExpand2 .form-control:eq(4)').val();
+        schema_setting_auto.vm = [];
+        var v0 = $.trim( $('.serviceAutoExpand2 .bs-example .btn-group:eq(0) button').text());
+        var v1 = $.trim($('.serviceAutoExpand2 .bs-example .btn-group:eq(1) button').text());
+        var lis0 = $('.serviceAutoExpand2 .bs-example .dropdown-menu:eq(0) li a');
+        var lis1 = $('.serviceAutoExpand2 .bs-example .dropdown-menu:eq(1) li a');
+        schema_setting_auto.vm.push(lis0.index(lis0.filter(function(){ return $(this).text() == v0;})))
+        schema_setting_auto.vm.push(lis1.index(lis1.filter(function(){ return $(this).text() == v1;})))
+
+        schema_setting_manual.nodeNum = $('.serviceAutoExpand3 .form-control:eq(0)').val();
+        schema_setting_manual.vm=[];
+        var c0 = $.trim( $('.serviceAutoExpand3 .bs-example .btn-group button:eq(0)').text());
+        var c1 = $.trim( $('.serviceAutoExpand3 .bs-example .btn-group button:eq(1)').text());
+        var c2 = $.trim( $('.serviceAutoExpand3 .bs-example .btn-group button:eq(2)').text());
+        var c3 = $.trim( $('.serviceAutoExpand3 .bs-example .btn-group button:eq(3)').text());
+        var c4 = $.trim( $('.serviceAutoExpand3 .bs-example .btn-group button:eq(4)').text());
+        var c5 = $.trim( $('.serviceAutoExpand3 .bs-example .btn-group button:eq(5)').text());
+        var lic0  = $('.serviceAutoExpand3 .bs-example .dropdown-menu:eq(0) li a');
+        var lic1  = $('.serviceAutoExpand3 .bs-example .dropdown-menu:eq(1) li a');
+        var lic2  = $('.serviceAutoExpand3 .bs-example .dropdown-menu:eq(2) li a');
+        var lic3  = $('.serviceAutoExpand3 .bs-example .dropdown-menu:eq(3) li a');
+        var lic4  = $('.serviceAutoExpand3 .bs-example .dropdown-menu:eq(4) li a');
+        var lic5  = $('.serviceAutoExpand3 .bs-example .dropdown-menu:eq(5) li a');
+        schema_setting_manual.vm.push(lic0.index(lic0.filter(function(){ return $(this).text() == c0;})))
+        schema_setting_manual.vm.push(lic1.index(lic1.filter(function(){ return $(this).text() == c1;})))
+        schema_setting_manual.vm.push(lic2.index(lic2.filter(function(){ return $(this).text() == c2;})))
+        schema_setting_manual.vm.push(lic3.index(lic3.filter(function(){ return $(this).text() == c3;})))
+        schema_setting_manual.vm.push(lic4.index(lic4.filter(function(){ return $(this).text() == c4;})))
+        schema_setting_manual.vm.push(lic5.index(lic5.filter(function(){ return $(this).text() == c5;})))
+
+
 
         data.threshold_setting =  JSON.stringify(threshold_setting);
         data.schema_setting_auto =  JSON.stringify(schema_setting_auto);
+        data.schema_setting_manual =  JSON.stringify(schema_setting_manual);
         data.id = 1;
 
         $.ajax({
@@ -69,7 +95,7 @@ $(function() {
             }
         });
     }
-    
+
 
     $('#resourceManageService1').on('click', function() {
         $('.expand').hide();
@@ -91,6 +117,9 @@ $(function() {
     $('#serviceAutoExpand2Save').on('click', function() {
         updateExpands();
     });
+    $('#serviceAutoExpand3Save').on('click', function() {
+        updateExpands();
+    })
 
     $('#resourceManageService1').click();
 
